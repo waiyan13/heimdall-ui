cf2ea4b0189abec5b98c123e24443af9b18b25a2
waiyan13 <zaw.waiyan.lwin96@gmail.com>
2024-12-23 16:43:54 +0700

    test[Login]: Test redirect to login

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,11 +1,40 @@
+import { Routes } from "@/config/routes";
 import { render, screen } from "@testing-library/react";
+import { userEvent } from "@testing-library/user-event";
+import { RouterProvider, createMemoryRouter } from "react-router";
 
 import App from "./App";
+import Login from "./features/login/Login";
+
+const routes = [
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: `/${Routes.login}`,
+    element: <Login />,
+  },
+];
+
+const router = createMemoryRouter(routes, {
+  initialEntries: ["/", `/${Routes.login}`],
+  initialIndex: 0,
+});
 
 describe("App", () => {
   it("should show login button", () => {
-    render(<App />);
+    render(<RouterProvider router={router} />);
 
     expect(screen.getByText("Login")).toBeInTheDocument();
+  });
+
+  it("should redirect to login screen", async () => {
+    const user = userEvent.setup();
+
+    render(<RouterProvider router={router} />);
+
+    await user.click(screen.getByText("Login"));
+    await screen.findByRole("button", { name: "Login" });
   });
 });
--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,15 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router";
 
 import App from "./App.tsx";
+import Login from "./features/login/Login";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(
+createRoot(document.getElementById("root") as HTMLElement).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route index element=<App /> />
+        <Route index element={<App />} />
+        <Route path="login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>,
